978072be9b80c40595942cb529225a45c8b76c45
ethanlolomg <ethanlolomg@gmail.com>
2023-01-31 23:17:07 -0800

    Deleted redundant end to end issuance and verification logic

--- packages/frontend/lib/builders/input-descriptors/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./helpers"
-export * from "./input-descriptor-builder"
-export * from "./input-descriptor-constraints-builder"
-export * from "./input-descriptor-constraints-field"
--- packages/frontend/lib/builders/output-descriptors/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./data-display-builder"
-export * from "./labeled-display-mapping-builder"
-export * from "./output-descriptor-builder"
