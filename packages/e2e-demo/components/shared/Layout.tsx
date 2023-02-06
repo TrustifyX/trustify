23a2c4ac73befae59c5b03c0629dfae6a687d19d
ethanlolomg <ethanlolomg@gmail.com>
2023-02-05 17:14:19 -0800

    nit

--- packages/e2e-demo/components/shared/Layout.tsx
+++ packages/e2e-demo/components/shared/Layout.tsx
@@ -73,7 +73,7 @@ const Layout: FC<Props> = ({ children, title, bgColor }) => {
             {children}
           </main>
           <footer className="py-8 text-sm text-center text-gray-00 text-extralight">
-            &copy;{new Date().getFullYear()} Centre | Software open sourced
+            &copy;{new Date().getFullYear()} Trustify | Software open sourced
             under the MIT license
           </footer>
         </div>
